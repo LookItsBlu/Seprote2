53316541b1aaf7a9f115f444b5349dbe730b2809
etibiss <33523974+etibiss@users.noreply.github.com>
2018-02-05 16:05:36 +0100

    05/02: Modif css + création squelette de liste_programme.php

--- s2-first_draft/login.php
+++ s2-first_draft/login.php
@@ -2,18 +2,22 @@
 	include('includes/header.php');
 ?>
 
+<link rel="stylesheet" type="text/css" href="css/login.css">
+
 <div class="loginWrap">
     <img class="loginSeprote" src="src/seprote.png" alt="Seprote" />
     <div class="loginIutImg">
         <img class="loginIut" src="src/iut.png" alt="IUT Calais Boulogne" />
     </div>
-    <div class="row">
-        <div class="form_connex col-md-offset-3 col-md-6">
-            <form method="POST" action="login.php" id="loginForm">
-                <input type="text" placeholder="Adresse e-mail" name="login" />
-                <input type="password" placeholder="Mot de passe" name="mdp" />
-                <button type="input">Connexion</button>
-            </form>
+    <div class="container">
+        <div class="row">
+            <div class="form_connex col-md-offset-3 col-md-6">
+                <form method="POST" action="login.php" id="loginForm">
+                    <input type="text" placeholder="Adresse e-mail" name="login" />
+                    <input type="password" placeholder="Mot de passe" name="mdp" />
+                    <button type="input">Connexion</button>
+                </form>
+            </div>
         </div>
     </div>
 
