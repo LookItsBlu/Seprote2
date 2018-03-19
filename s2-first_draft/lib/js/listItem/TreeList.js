f1106de367333346aed1cfd1d491f80c337c6ccd
etibiss <33523974+etibiss@users.noreply.github.com>
2018-03-19 20:47:56 +0100

    19/03: Amélioriation css suite au changement de liste_programmes

--- s2-first_draft/lib/js/listItem/TreeList.js
+++ s2-first_draft/lib/js/listItem/TreeList.js
@@ -32,7 +32,7 @@ export default class TreeList {
     }
 
     nextLevel() {
-        if(this.depth == this.niveaux.length-1) {
+        if (this.depth == this.niveaux.length - 1) {
             return 0;
         } else {
             this.depth++;
@@ -45,14 +45,14 @@ export default class TreeList {
     }
 
     previousLevel() {
-        if(this.depth == 0) {
+        if (this.depth == 0) {
             return 0;
         } else {
             this.depth--;
 
             //we're going back a level in the list, pop the last visited id
             this.history.pop();
-            this.selectedId = this.history[this.history.length-1];
+            this.selectedId = this.history[this.history.length - 1];
 
             this.getItems();
         }
@@ -64,10 +64,13 @@ export default class TreeList {
         $.ajax({
             method: 'post',
             url: 'lib/js/listItem/php/treeList.getItems.php',
-            data: { 'depth': p.depth, 'id': p.selectedId },
-            success: function(data){
+            data: {
+                'depth': p.depth,
+                'id': p.selectedId
+            },
+            success: function (data) {
                 p.received_data = JSON.parse(data);
-                if(p.received_data.length>0) {
+                if (p.received_data.length > 0) {
                     p.createItems();
                 }
             }
@@ -79,17 +82,17 @@ export default class TreeList {
 
         p.items = new Array();
 
-        var i=0;
+        var i = 0;
         do {
             var value_to_display = p.received_data[i];
             var date_debut, date_fin;
 
             // si la valeur que l'on veut retourner est une liste de 2 valeurs,
             // c'est une année, on va donc rendre ca plus lisible
-            if(p.displayed_value[ p.depth ].constructor === Array) {
+            if (p.displayed_value[p.depth].constructor === Array) {
                 // pour avoir l'année uniquement, utiliser .substring(0, 4)
-                date_debut = value_to_display[p.displayed_value[ p.depth ][ 0 ]];
-                date_fin = value_to_display[p.displayed_value[ p.depth ][ 1 ]];
+                date_debut = value_to_display[p.displayed_value[p.depth][0]];
+                date_fin = value_to_display[p.displayed_value[p.depth][1]];
 
                 value_to_display = '..nom.. ..debut.. - ..fin..'.strcast({
                     'nom': p.niveaux[p.depth],
@@ -97,13 +100,13 @@ export default class TreeList {
                     'fin': date_fin
                 });
             } else {
-                value_to_display = value_to_display[p.displayed_value[ p.depth ]];
+                value_to_display = value_to_display[p.displayed_value[p.depth]];
             }
 
-            p.items.push( new ListItem(p, value_to_display, p.received_data[i][0], p.history) );
+            p.items.push(new ListItem(p, value_to_display, p.received_data[i][0], p.history));
 
             i++;
-        } while(i<p.received_data.length);
+        } while (i < p.received_data.length);
 
         this.createTable();
     }
@@ -121,7 +124,7 @@ export default class TreeList {
         p.base.innerHTML += "<tbody></tbody>";
 
         //ajoute chaque item dans la liste
-        p.items.forEach( elem => {
+        p.items.forEach(elem => {
             p.base.children[1].innerHTML += "..item..".strcast({
                 "item": elem.displayItem()
             });
@@ -132,16 +135,21 @@ export default class TreeList {
             elem.setClick();
         });
         //ajoute un button pour remonter la liste si nous ne somme pas a la base de ce dernier
-        if(p.history.length > 1) {
+        if (p.history.length > 1) {
             var backBtn = document.createElement("tr"),
                 backBtnCell = document.createElement("td"),
-                backTxt = document.createTextNode("retour");
+                backImg = document.createElement("img"),
+                backTxt = document.createTextNode("Retour");
+            backImg.src = "src/arrow-return-left.png";
             backBtn.appendChild(backBtnCell);
+            backBtnCell.appendChild(backImg);
             backBtnCell.appendChild(backTxt);
             backBtn.className = 'treelist-history-back';
             p.base.children[1].insertBefore(backBtn, p.base.children[1].children[0]);
 
-            document.querySelector('.treelist-history-back').addEventListener('dblclick', ()=>{ p.previousLevel(); });
+            document.querySelector('.treelist-history-back').addEventListener('dblclick', () => {
+                p.previousLevel();
+            });
         }
     }
 }
