57479d191d2d3478798a522ff1f6b9e128f2ef85
Lenglet Anthony <anthony-62100@hotmail.fr>
2018-03-21 00:55:46 +0100

    updated gestion_compte to recent changes

--- s2-first_draft/gestion_compte.php
+++ s2-first_draft/gestion_compte.php
@@ -12,62 +12,73 @@ include('includes/header.php');
 include('includes/menu.php');
 ?>
 
-<div id="main">
-    <div class="mainWrap">
-        <div id="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 accInput">
-                        <form class='addCompte' action="#" method="POST">
-                            <div class="container">
-                                <div class="row">
-                                    <input class="col-md-2" name="nom" placeholder="Nom" type="text">
-                                    <input class="col-md-2" name="prenom" placeholder="Prénom" type="text">
-                                    <input class="col-md-2" name="mail" placeholder="Adresse mail" type="text">
-                                    <select class="col-md-2" name="id_role" type="text">
-                                        <?php if($_SESSION['role'] == 1) { ?>
-                                        <option value="2">Gestionnaire</option>
-                                        <?php } ?>
-                                        <option value="3" selected>Professeur</option>
-                                    </select>
-                                    <input class="col-md-2" type="password" name="mdp" placeholder="Mot de passe">
-                                    <?php
-                                    $req_dptm = $bdd->prepare("SELECT id_d,nom_d from departement");
-                                    $req_dptm->execute();
-                                    ?>
-                                    <button class="col-md-1" class="submitBtn" value="Créer" type="submit">Ajouter</button>
-                                    <div class="form-check col-md-12">
-                                        <span>Département :</span>
-                                        <?php while($data =$req_dptm->fetch()){ ?>
-                                        <input type="checkbox" class="" id="dptm<?php echo $data['id_d'];?>">
-                                        <label class="" for="dptm<?php echo $data['id_d'];?>"><?php echo $data['nom_d'];?></label>
-                                        <?php } ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+<div id="main" class="mainWrap">
+    <div id="content" class="container">
+        <div class="row">
+            <div class="col-md-12 accInput">
+                <form class='addCompte' action="#" method="POST">
+                    <input class="col-md-2" name="nom" placeholder="Nom" type="text">
+                    <input class="col-md-2" name="prenom" placeholder="Prénom" type="text">
+                    <input class="col-md-2" name="mail" placeholder="Adresse mail" type="text">
+                    <select class="col-md-2" name="id_role" type="text">
+                        <?php if($_SESSION['role'] == 1) { ?>
+                            <option value="2">Gestionnaire</option>
+                        <?php } ?>
+                        <option value="3" selected>Professeur</option>
+                    </select>
+                    <input class="col-md-2" type="password" name="mdp" placeholder="Mot de passe">
+                    <?php
+                        $req_dptm = $bdd->prepare("SELECT id_d,nom_d from departement");
+                        $req_dptm->execute();
+                    ?>
+                    <button class="col-md-1" class="submitBtn" value="Créer" type="submit">Ajouter</button>
+                    <div class="form-check col-md-12">
+                        <span>Département :</span>
+                        <?php while($data =$req_dptm->fetch()){ ?>
+                            <input type="checkbox" class="" id="dptm<?php echo $data['id_d'];?>">
+                            <label class="" for="dptm<?php echo $data['id_d'];?>"><?php echo $data['nom_d'];?></label>
+                        <?php } ?>
                     </div>
-                </div>
-                <div class="row">
-                    <table id="ListeComptes" class="sep_table col-md-12">
-                        <form id="delete_users" method="POST" action="#">
-                            <input type='hidden' name='type_form' value='3'>
-                            <thead>
-                                <tr>
-                                    <th>Nom</th>
-                                    <th>Prénom</th>
-                                    <th>Adresse mail</th>
-                                    <th>Rôle</th>
-                                    <th>Département</th>
-                                    <th>Éditer</th>
-                                    <th>Supprimer</th>
-                                </tr>
-                            </thead>
-                            <tbody></tbody>
-                        </form>
-                    </table>
-                </div>
+                </form>
             </div>
+        </div>
+        <div class="row">
+            <table id="ListeComptes" class="sep_table col-md-12">
+                <form id="delete_users" method="POST" action="#">
+                    <input type='hidden' name='type_form' value='3'>
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th>Adresse mail</th>
+                            <th>Rôle</th>
+                            <th>Département</th>
+                            <th>Éditer</th>
+                            <th>Supprimer</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </form>
+            </table>
+        </div>
+        <div class="row">
+            <table id="ListeComptes" class="sep_table col-md-12">
+                <form id="delete_users" method="POST" action="#">
+                    <input type='hidden' name='type_form' value='3'>
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th>Adresse mail</th>
+                            <th>Rôle</th>
+                            <th>Département</th>
+                            <th>Éditer</th>
+                            <th>Supprimer</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </form>
+            </table>
         </div>
     </div>
 </div>
