fb297df714be9f787d3d87fc4c17136746421901
LookItsBlu <anthony-62100@hotmail.fr>
2018-03-21 12:58:44 +0100

    Add "Add item" functionality

--- s2-first_draft/index.php
+++ s2-first_draft/index.php
@@ -4,6 +4,8 @@
 	require_once "bdd/BDD.php";
 
 	if(isset($_POST['id_d'])) {
+		$_SESSION['id_dep'] = $_POST['id_d'];
+
 		$req_prog = $bdd->prepare("SELECT p.id_prog, p.prog_nom
 								FROM programme p, prog_for pf, formation f, departement d
 								WHERE p.id_prog=pf.id_prog
@@ -25,25 +27,26 @@
 
 <div id="main">
     <div class="mainWrap">
-        <h1 class="bonjour">Bonjour, <?= htmlspecialchars($_SESSION['nom']). " " . htmlspecialchars($_SESSION['prenom']) ?>.</h1>
-		<br>
-		<span class="selectDepText">Choix du département: </span>
-        <select class="selectDep">
-            <option style="display:none"></option>
-            <?php
-                $req_dep = $bdd->prepare("SELECT d.id_d, d.nom_d
-										FROM departement d, utilisateur u, util_dep ud
-										WHERE u.id_u=ud.id_u
-										AND ud.id_d=d.id_d
-										AND u.id_u=:id");
-				$req_dep->bindValue(':id', $_SESSION['id']);
-				$req_dep->execute();
-                while($donnees = $req_dep->fetch()){
-                    echo "<option value='".$donnees["id_d"]."'>".$donnees["nom_d"]."</option>";
-                }
-            ?>
-        </select>
-        <br>
+        <h1 class="bonjour">Bonjour, <?= htmlspecialchars($_SESSION['nom'])." ".htmlspecialchars($_SESSION['prenom']) ?>.</h1>
+		<div id="breadcrumb"></div>
+		<div id="dep">
+			<span class="selectDepText">Choix du département: </span>
+			<select class="selectDep">
+				<option style="display:none"></option>
+				<?php
+					$req_dep = $bdd->prepare("SELECT d.id_d, d.nom_d
+											FROM departement d, utilisateur u, util_dep ud
+											WHERE u.id_u=ud.id_u
+											AND ud.id_d=d.id_d
+											AND u.id_u=:id");
+					$req_dep->bindValue(':id', $_SESSION['id']);
+					$req_dep->execute();
+					while($donnees = $req_dep->fetch()){
+						echo "<option value='".$donnees["id_d"]."'>".$donnees["nom_d"]."</option>";
+					}
+				?>
+			</select>
+		</div>
         <div id="hidden">
 			<span class="selectProgText">Choix du programme: </span>
 			<select class="selectProg">
@@ -51,24 +54,25 @@
 
 			</select>
 		</div>
+		<div id="hidden2">
+			<div id="content">
+				<div id="left">
+					<div class="center">
+						<div id="chart"></div>
+						<div class="selection">
+							<button class="tabBtn">Tableau</button>
+							<button class="pieBtn">Circulaire</button>
+							<button class="barBtn">Barres</button>
+						</div>
+					</div>
+				</div>
 
-        <div id="content">
-            <div id="left">
-                <div class="center">
-                    <div id="chart"></div>
-                    <div class="selection">
-                        <button class="tabBtn">Tableau</button>
-                        <button class="pieBtn">Circulaire</button>
-                        <button class="barBtn">Barres</button>
-                    </div>
-                </div>
-            </div>
-
-            <div id="right">
-                <div id="calendar">
-                </div>
-            </div>
-        </div>
+				<div id="right">
+					<div id="calendar">
+					</div>
+				</div>
+			</div>
+		</div>
     </div>
 </div>
 
@@ -76,6 +80,7 @@
 <script>
 	$(document).ready(function() {
 		$("#hidden").hide();
+		$("#hidden2").hide();
 		$(".selectDep").change(function() {
 			params = "id_d="+$(this).val();
 			$.ajax({
@@ -88,6 +93,12 @@
 				}
 			});
 			return false;
+		});
+		$(".selectProg").change(function() {
+			$("#dep").hide();
+			$("#hidden").hide();
+			$("#breadcrumb").html("<p><b>"+$(".selectDep").text()+"</b> > <b>"+$(".selectProg").text()+"</b> > Heures</p>");
+			$("#hidden2").show();
 		});
 	});
 </script>
--- s2-first_draft/lib/infoList/php/infoList.addItem.php
+++ s2-first_draft/lib/infoList/php/infoList.addItem.php
@@ -1,60 +1,82 @@
 <?php
-include ('../../../bdd/BDD.php');
+	include ('../../../bdd/BDD.php');
 
-/*
-$_POST['breadcrum']:
-[0] => id_annee
-[1] => id_formation
-[2] => id_semestre
-[3] => id_periode
-[4] => id_module
+	/*
+	$_POST['breadcrum']:
+	[0] => id_annee
+	[1] => id_formation
+	[2] => id_semestre
+	[3] => id_periode
+	[4] => id_module
 
-$_POST['new_values']:
-semestres, periodes:
-[0] => date debut
-[1] => date fin
-modules:
-[0] => nom
-[1] => code
-le reste:
-[0] => nom
-*/
+	$_POST['new_values']:
+	semestres, periodes:
+	[0] => date debut
+	[1] => date fin
+	modules:
+	[0] => nom
+	[1] => code
+	le reste:
+	[0] => nom
+	*/
 
-$res = '';
+	$res = '';
 
-switch(count($_POST['breadcrum'])-1){
-    case 0: // formation
-        $sql = 'INSERT INTO formation(nom_f, id_d) VALUES(?, 1)';
-        $pdoreq = $bdd->prepare($sql);
-        $pdoreq->execute($_POST['new_values'][0]);
+	switch(count($_POST['breadcrum'])-1){
+		case 0: // formation
+			$sql = 'INSERT INTO formation(nom_f, id_d) VALUES(?, 1)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute($_POST['new_values'][0]);
 
-        $sql = 'INSERT INTO for_annee VALUES(:id_formation, :id_annee)';
-        $pdoreq = $bdd->prepare($sql);
-        $pdoreq->execute(array(
-            ':id_annee' => $_POST['breadcrum'][0],
-            ':id_formation' => $bdd->lastInsertId()
-        ));
-        break;
-    case 1: // semestre
-        $sql = 'INSERT INTO semestre() VALUES()';
-        $pdoreq = $bdd->prepare($sql);
-        $pdoreq->execute($_POST['new_values'][0]);
+			$sql = 'INSERT INTO for_annee(id_f, id_a) VALUES(:id_formation, :id_annee)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute(array(
+				':id_annee' => $_POST['breadcrum'][0],
+				':id_formation' => $bdd->lastInsertId()
+			));
+			break;
+		case 1: // semestre
+			$sql = 'INSERT INTO semestre(dat_deb_s, dat_fin_s, id_prog) VALUES(:dat_deb, :dab_fin, :dept)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute(array(
+				':dat_deb' => $_POST['new_values'][0],
+				':dab_fin' => $_POST['new_values'][1],
+				':dept' => $_SESSION['id_dep']
+			));
+			break;
+		case 2: // periode
+			$sql = 'INSERT INTO periode(dat_deb_p, dat_fin_p) VALUES(:dat_deb, :dab_fin)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute(array(
+				':dat_deb' => $_POST['new_values'][0],
+				':dab_fin' => $_POST['new_values'][1]
+			));
 
-        $sql = 'INSERT INTO for_annee VALUES(:id_formation, :id_annee)';
-        $pdoreq = $bdd->prepare($sql);
-        $pdoreq->execute(array(
-            ':id_annee' => $_POST['breadcrum'][0],
-            ':id_formation' => $bdd->lastInsertId()
-        ));
-        break;
-    case 2: // periode
+			$sql = 'INSERT INTO per_sem(id_per, id_sem) VALUES(:id_periode, :id_semestre)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute(array(
+				':id_semestre' => $_POST['breadcrum'][2],
+				':id_periode' => $bdd->lastInsertId()
+			));
+			break;
+		case 3: // module
+			$sql = 'INSERT INTO module(nom_m, code_m) VALUES(:nom, :code)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute(array(
+				':nom' => $_POST['new_values'][0],
+				':code' => $_POST['new_values'][1]
+			));
 
-    case 3: // module
+			$sql = 'INSERT INTO per_mod(id_per, id_mod) VALUES(:id_periode, :id_module)';
+			$pdoreq = $bdd->prepare($sql);
+			$pdoreq->execute(array(
+				':id_periode' => $_POST['breadcrum'][3],
+				':id_module' => $bdd->lastInsertId()
+			));
+			break;
+		default:
+			break;
+	}
 
-    case 4: // heures
-
-    default:
-        break;
-}
-
-echo(true); //json_encode($res)
+	echo(true); //json_encode($res)
+?>
